d656f8ae9d4be8b1719bb31b27f9e9a35d98deef
zulhamdi <sosmedzulham@gmail.com>
2023-09-15 13:42:03 +0700

    menambahkan animasi typing

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const menuList = document.getElementById('menu-h')
 menuIcon.addEventListener('click', () => {
     menuList.classList.toggle('hidden');
 })
+
+var typed = new Typed(".auto-type",{
+    strings: ['HELLO,','BONJOUR', 'HOLA,','NI HAO,'],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+})
