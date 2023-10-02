d3b01780a1c52315452628304bb4121b217ffedf
zulhamdi <sosmedzulham@gmail.com>
2023-10-02 09:58:46 +0700

    mengembalikan porto pribadi ke semula

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById('hamburger-menu')
+const menuList = document.getElementById('menu-h')
+menuIcon.addEventListener('click', () => {
+    menuList.classList.toggle('hidden');
+})
+
+var typed = new Typed(".auto-type",{
+    strings: ['HELLO,','BONJOUR,', 'HOLA,','NI HAO,'],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+})
