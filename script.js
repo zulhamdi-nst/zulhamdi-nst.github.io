010a248a32b96ac987c2d9018f4f5ebb53bc9acd
zulhamdi <sosmedzulham@gmail.com>
2024-04-25 10:36:22 +0700

    memperbaiki section

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// if(confirm("Apakah anda ingin memasukan nama anda ?")){
-//     let inputName = prompt("MASUKAN NAMA ANDA");
-//     let kapital = inputName.toUpperCase();
-//     alert("SELAMAT DATANG DI WEB SAYA "+kapital+" BODOH")
-// } else {
-//     alert("MASOK KAN LAH BODOH !");
-// };
-
 const menuIcon = document.getElementById('hamburger-menu')
 const menuList = document.getElementById('menu-h')
 menuIcon.addEventListener('click', () => {
